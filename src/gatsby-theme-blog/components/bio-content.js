65841033a93fbee0a4d6c34d19b6993eecc8b667
nolan.sedley <nolan.sedley@mindbodyonline.com>
2020-06-08 16:49:53 -0700

    Update bio content with introduction

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,17 +8,23 @@ import { Styled } from "theme-ui"
 export default function Bio() {
   return (
     <>
-      I am Nolan and currently develop as a software engineering manager at{" "}
-      <Styled.a href="https://mindbody.io" target="blank">Mindbody</Styled.a>. We build
-      software to connect the world to wellness.
-      <br />
-      The majority of my articles are regarding .NET and JavaScript development
-      though I also dabble in Java, Ruby and Python. Find more about my career
-      experience and ongoing projects on{" "}
+      Hello, I am Nolan! I currently develop as a software engineering manager
+      at{" "}
+      <Styled.a href="https://mindbody.io" target="blank">
+        Mindbody
+      </Styled.a>{" "}
+      where we build software to connect the world to wellness. The majority of
+      my articles are regarding .NET and JavaScript development though I also
+      dabble in Java, Ruby and Python. Find more about my career experience and
+      ongoing projects on{" "}
       <Styled.a href="https://www.linkedin.com/in/nsedley/" target="blank">
         LinkedIn
       </Styled.a>{" "}
-      and <Styled.a href="https://github.com/strake7" target="blank">GitHub</Styled.a>.      
+      and{" "}
+      <Styled.a href="https://github.com/strake7" target="blank">
+        GitHub
+      </Styled.a>
+      .
     </>
   )
 }
